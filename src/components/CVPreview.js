c94b7b83c7b591625a4dfa18e78ab47439481abb
jeff <jeffreyjohn102@gmail.com>
2023-02-27 18:37:57 -0500

    Create initial layout and style for CVPreview component

--- src/components/CVPreview.js
+++ src/components/CVPreview.js
@@ -7,7 +7,30 @@ class CVPreview extends Component {
     render() {
         return (
             <div className='cv-preview'>
+                <div className='main-page'>
+                    <header>
+                        <h1 className='name-head'>Name Name</h1>
+                        <div className='info-head'>
+                            <p>email</p>
+                            <p>phone</p>
+                            <p>address</p>
+                        </div>
+                    </header>
+                    <section className='summary'>
+                        <p>
+                            vdskvdksmvdsmvmdslmvmdsvmldsvmdsmvmdsvmdsvmkdsmvklfs vhbwfencmsmcsvdslv svldsvdv slvdk s
+                        </p>
 
+                    </section>
+                    <section className='education'>
+                        <h2>Education</h2>
+
+                    </section>
+                    <section className='experience'>
+                        <h2>Experience</h2>
+                        
+                    </section>
+                </div>
             </div>
         );
     }
